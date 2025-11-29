f83cba8caea088a14b16073aaad6e5b1a5f0668c
Trần Xuân Nam <30828722+dekapro9x@users.noreply.github.com>
2025-11-29 16:28:02 +0700

    Setting: create file bridging header connect /nfc-Bridging-Header.h call native module IOS

---
+++ ios/Dummy.swift
@@ -0,0 +1,8 @@
+//
+//  Dummy.swift
+//  nfc
+//
+//  Created by Trần Xuân Nam on 29/11/25.
+//
+
+import Foundation
---
+++ ios/nfc-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  nfc-Bridging-Header.h
+//  nfc
+//
+//  Created by Trần Xuân Nam on 29/11/25.
+//
+
+#ifndef nfc_Bridging_Header_h
+#define nfc_Bridging_Header_h
+#import <React/RCTBridgeModule.h>
+#endif /* nfc_Bridging_Header_h */
